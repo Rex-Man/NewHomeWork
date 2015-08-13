25b41346ae143a9813ee9f5d74f8064c3e0608fc
rex <rex.man@oocl.com>
2015-08-13 18:11:03 +0800

    Initial Commit

---
+++ NewHomeWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewHomeWork
+//
+//  Created by admin on 8/13/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewHomeWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewHomeWork
+//
+//  Created by admin on 8/13/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
