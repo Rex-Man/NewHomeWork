0e4d366b8dff3c2575430e99877b9c7db040b2f8
rex <rex.man@oocl.com>
2015-08-20 17:41:00 +0800

    add

---
+++ NewHomeWork/CityModel/CityModel.h
@@ -0,0 +1,13 @@
+//
+//  CityModel.h
+//  NewHomeWork
+//
+//  Created by admin on 8/20/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#ifndef NewHomeWork_CityModel_h
+#define NewHomeWork_CityModel_h
+
+
+#endif
---
+++ NewHomeWork/CityModel/CityModel.m
@@ -0,0 +1,9 @@
+//
+//  CityModel.m
+//  NewHomeWork
+//
+//  Created by admin on 8/20/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
