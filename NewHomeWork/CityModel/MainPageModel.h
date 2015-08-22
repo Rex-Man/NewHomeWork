7179b267ad6406b218d68f115fd63be61afb1d02
REX MAN (EUCD-EUC-ISD-OOCLL/ZHA) <rex.man@oocl.com>
2015-08-22 19:57:19 +0800

    change

---
+++ NewHomeWork/CityModel/MainPageModel.h
@@ -0,0 +1,13 @@
+//
+//  MainPageModel.h
+//  NewHomeWork
+//
+//  Created by REX MAN (EUCD-EUC-ISD-OOCLL/ZHA) on 8/21/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#ifndef NewHomeWork_MainPageModel_h
+#define NewHomeWork_MainPageModel_h
+
+
+#endif
---
+++ NewHomeWork/CityModel/MainPageModel.m
@@ -0,0 +1,9 @@
+//
+//  MainPageModel.m
+//  NewHomeWork
+//
+//  Created by REX MAN (EUCD-EUC-ISD-OOCLL/ZHA) on 8/21/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
